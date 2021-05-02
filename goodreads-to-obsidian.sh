9cb4b81839f761f0a037c83ac857677ec684e6ab
selfire1 <try2banonym@gmail.com>
2021-05-02 15:59:26 +0200

    init

---
+++ goodreads-to-obsidian.sh
@@ -0,0 +1,211 @@
+#!/bin/sh
+
+# Enter urls to your goodreads rss feed below.
+# You can find it by navigating to one of your goodreads shelves and
+# clicking the "RSS" button at the bottom of the page.
+
+# url for "Currently reading":
+url="https://www.goodreads.com/review/list_rss/105006095?key=P_HhmmxjM6_vnVCBTApHhsL5lrN4ygoN9aTiP_sPxCP2cdda&shelf=currently-reading"
+# url for "Read":
+readurl="https://www.goodreads.com/review/list_rss/105006095?key=P_HhmmxjM6_vnVCBTApHhsL5lrN4ygoN9aTiP_sPxCP2cdda&shelf=read"
+
+# Enter the path to your Vault
+vaultpath="$HOME/Dropbox/Vault"
+#vaultpath="$HOME/Desktop/Vault"
+
+# Assign times to variables
+year=$(date +%Y)
+nummonth=$(date +%m)
+month=$(date +%B)
+
+# Grab the data from CURRENTLY-READING feed and format it
+IFS=$'\n' feed=$(curl --silent "$url" | grep -E '(title>|book_large_image_url>|author_name>|book_published>|book_id>)' | \
+sed -e 's/<!\[CDATA\[//' -e 's/\]\]>//' \
+-e 's/Joschua.s bookshelf: currently-reading//' \
+-e 's/<book_large_image_url>//' -e 's/<\/book_large_image_url>/ | /' \
+-e 's/<title>//' -e 's/<\/title>/ | /' \
+-e 's/<author_name>//' -e 's/<\/author_name>/ | /' \
+-e 's/<book_published>//' -e 's/<\/book_published>/ | /' \
+-e 's/<book_id>//' -e 's/<\/book_id>/ | /' \
+-e 's/^[ \t]*//' -e 's/[ \t]*$//' | \
+tail +3 | \
+fmt
+)
+
+
+# Grab the bookid from READ data from the url and format it
+IFS=$'\n' readfeed=$(curl --silent "$readurl" | grep -E '(book_id>)' | \
+sed -e 's/<book_id>//' -e 's/<\/book_id>/ | /' \
+-e 's/^[ \t]*//' -e 's/[ \t]*$//' | \
+fmt
+)
+
+# Turn the data into an array
+arr=($(echo $feed | tr "|" "\n")) # CURRENTLY-READING
+readarr=($(echo $readfeed | tr "|" "\n")) # READ
+
+# Remove whitespace on each element: CURRENTLY-READING
+for (( i = 0 ; i < ${#arr[@]} ; i++ ))
+do
+  arr[$i]=$(echo "${arr[$i]}" | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+done
+
+# Remove whitespace on each element: READ
+for (( i = 0 ; i < ${#readarr[@]} ; i++ ))
+do
+  readarr[$i]=$(echo "${readarr[$i]}" | sed -e 's/^[ \t]*//' -e 's/[ \t]*$//')
+done
+
+
+# Delete elements that were found in output.txt
+#  for (( i = 0 ; i < ${savedlength} ; i++ ))
+#    do
+#      if grep -Fxq "${arr[$i]}" output.txt
+#        then
+#          # code if found
+#          unset arr[$i]
+#         #else
+#         # code if not found
+#      fi
+#    done
+
+# Delete elements found in existing notes
+# Get the amount of books by dividing array by 5
+bookamount=$( expr "${#arr[@]}" / 5)
+
+for (( i = 0 ; i < ${bookamount} ; i++ ))
+do
+  # Create a temporary counter to loop through books
+  counter=$( expr "$i" \* 5)
+
+  # Set variables
+  bookid=${arr[$( expr "$counter" + 1)]}
+
+# Check if book already exists in note by bookid
+    #if find "${vaultpath}" -type f -name "*.md" -exec grep -qn "${bookid}" {} \+
+    # if grep -Fxq "${bookid}" -r "${vaultpath}/"
+    if grep -q "${bookid}" -r "${vaultpath}"
+      then
+        # code if found
+          unset arr["$counter"]
+          unset arr[$( expr "$counter" + 1)]
+          unset arr[$( expr "$counter" + 2)]
+          unset arr[$( expr "$counter" + 3)]
+          unset arr[$( expr "$counter" + 4)]
+
+       # code if not found
+
+     fi
+done
+
+# Reindex array to take away gaps
+for i in "${!arr[@]}"; do
+    new_array+=( "${arr[i]}" )
+done
+arr=("${new_array[@]}")
+unset new_array
+
+# Get the amount of books by dividing array by 5
+bookamount=$( expr "${#arr[@]}" / 5)
+
+if (( "$bookamount" == 0 )); then
+  osascript -e "display notification \"No new books found.\" with title \"Currently-reading: No update\""
+fi
+
+# Start the loop for each book
+for (( i = 0 ; i < ${bookamount} ; i++ ))
+do
+
+  counter=$( expr "$i" \* 5)
+
+  # Set variables
+  title=${arr["$counter"]}
+  bookid=${arr[$( expr "$counter" + 1)]}
+  imglink=${arr[$( expr "$counter" + 2)]}
+  author=${arr[$( expr "$counter" + 3)]}
+  pub=${arr[$( expr "$counter" + 4)]}
+
+
+# Delete illegal (':' and '/') and unwanted ('#') characters
+cleantitle=$(echo "${title}" | sed -e 's/\///' -e 's/:/ –/' -e 's/#//')
+
+  # Write the contents for the book file
+
+  if [[ "$cleantitle" == "" ]];
+  then
+    osascript -e "display notification \"Failed to create note due to empty array.\" with title \"Error!\""
+  else
+    echo "---
+bookid: ${bookid}
+---
+
+links: [[Books MOC]]
+#currently-reading
+# ${title}
+![b|150](${imglink})
+* Type: #book/
+* Universe/Series: ADD SERIES
+* Author: [[${author}]]
+* Year published: [[${pub}]]" >> "${vaultpath}/${cleantitle}.md"
+
+    # Display a notification when creating the file
+    osascript -e "display notification \"Booknote created!\" with title \"${cleantitle//\"/\\\"}\""
+  fi
+
+done
+
+ifbookid=$(find "${vaultpath}" -type f -print0 | xargs -0 grep -li "${cbookid}")
+ifcurrread=$(find "${vaultpath}" -type f -print0 | xargs -0 grep -li "#currently-reading")
+
+if find "${vaultpath}" -type f -print0 | xargs -0 grep -li "${cbookid}"
+then
+  # Code if found: update read books
+  fname=$(find "${vaultpath}" -type f -print0 | xargs -0 grep -li "${cbookid}")
+  sed -i '' "/Year published: \[\[[0-9][0-9][0-9][0-9]\]\]/ a\\
+  \* Year read: #read${year}" "$fname"
+  sed -i '' "/Year read: #read${year}/ a\\
+  \* Month read: [[${year}-${nummonth}-${month}|${month} ${year}]]" "$fname"
+  sed -i '' -e 's/#currently-reading/#outline \/ #welcome/' "$fname"
+
+  # Grab the name of the changed book
+  fname=$(echo ${fname} | sed 's/^.*\///' | sed 's/\.[^.]*$//')
+  osascript -e "display notification \"${fname}\" with title \"Updated read books\""
+else
+ # code if not found: No new books
+ osascript -e "display notification \"No new read books.\" with title \"Read: No update\""
+fi
+
+for (( i = 0 ; i < ${#readarr[@]} ; i++ ))
+do
+  #circle through bookid array
+  cbookid=${readarr["$i"]}
+
+  # If in the path to the vault, there is a file with the current id, then …
+  if find "${vaultpath}" -not -path "*/\.*" -type f \( -iname "*.md" \) -print0 | xargs -0 grep -li "${cbookid}"
+  then
+  # … set variable fname to that file
+  fname=$(find "${vaultpath}" -not -path "*/\.*" -type f \( -iname "*.md" \) -print0 | xargs -0 grep -li "${cbookid}")
+    # Check if it has tag "#currently-reading"
+      if grep "#currently-reading" "${fname}"
+      then
+        # If yes, change the formatting, delete the "#currently-reading" tag
+        sed -i '' "/Year published: \[\[[0-9][0-9][0-9][0-9]\]\]/ a\\
+        \* Year read: #read${year}" "$fname"
+        sed -i '' "/Year read: #read${year}/ a\\
+        \* Month read: [[${year}-${nummonth}-${month}|${month} ${year}]]" "$fname"
+        sed -i '' -e 's/#currently-reading/#outline \/ #welcome/' "$fname"
+
+        # Grab the name of the changed book
+        declare -i updatedbooks; updatedbooks+=1
+        fname=$(echo ${fname} | sed 's/^.*\///' | sed 's/\.[^.]*$//')
+        # Show notification
+        osascript -e "display notification \"${fname}\" with title \"Updated read books\""
+      fi
+  fi
+done
+
+# code if not found: No new books
+if [[ ${updatedbooks} = "" ]]
+then
+osascript -e "display notification \"No new read books.\" with title \"Read: No update\""
+fi
